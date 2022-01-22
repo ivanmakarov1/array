a91b31e0f183dafc1d115bce69d28e5930993604
ivanmakarov1 <ivanmakarov1@yandex.ru>
2022-01-22 13:36:17 +0300

    3

--- unit_10.js
+++ unit_10.js
@@ -15,13 +15,14 @@ document.querySelector(".b-1").onclick = f1;
 //Создайте массив ar2 содержащий строки, числа, булевы значения. Выведите его в div.out-2. Используйте шаблон вывода из кода в JS. Вывод - по нажатию кнопки b-2
 
 // let ar2 = // переменную обьявляем здесь!!!!
+let ar2 = [false, " ", 100334, " ", "data structure", " ", 69.569];
 
 function f2() {
-  // let out = '';
-  // for (let i = 0; i < ar2.length; i++) {
-  //     out +=ar2[i]+' ';
-  // }
-  // document.querySelector('.out-2').innerHTML = out;
+  let out = "";
+  for (let i = 0; i < ar2.length; i++) {
+    out += ar2[i] + " ";
+  }
+  document.querySelector(".out-2").innerHTML = out;
 }
 
 document.querySelector(".b-2").onclick = f2;
